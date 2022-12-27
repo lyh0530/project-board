5426508099cede812a54d229cabede5985793a77
lyh <jj2221@naver.com>
2022-12-27 14:38:55 +0900

    article,articlecomments java 생성

---
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -0,0 +1,12 @@
+package com.koreait.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
