e60df11af842573f8f1c1ee9392aafd31a74bd7c
lyh <jj2221@naver.com>
2022-12-28 09:31:04 +0900

    #7 - 도메인 클래스 설계
    게시글, 댓글 설계

--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
+    private Article article;
     private String content;
     private LocalDateTime createdAt;
     private String createdBy;
