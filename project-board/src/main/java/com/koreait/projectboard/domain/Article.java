1bc1a7f0e1810524ce3bd52c519af903afb5b34a
lyh <jj2221@naver.com>
2022-12-28 10:32:13 +0900

    클래스설계 수정

--- project-board/src/main/java/com/koreait/projectboard/domain/Article.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/Article.java
@@ -1,12 +1,27 @@
 package com.koreait.projectboard.domain;
 
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Index;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
 public class Article {
     private Long id;
     private String title;
     private String content;
     private String hashtag;
+
+
     private LocalDateTime createdAt;
     private String createdBy;
     private LocalDateTime modifiedAt;
--- project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/koreait/projectboard/domain/ArticleComment.java
@@ -6,6 +6,7 @@ public class ArticleComment {
     private Long id;
     private Article article;
     private String content;
+
     private LocalDateTime createdAt;
     private String createdBy;
     private LocalDateTime modifiedAt;
